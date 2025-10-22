f27ef4fb006cdf83f2a1078ab33e8414562b2ce1
kahqgc <kahqgc@gmail.com>
2025-10-22 11:06:03 -0500

    remove accidentally added folder

--- backend/src/main/java/com/example/event_schedule/repositories/InstructorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.event_schedule.repositories;
-
-import com.example.event_schedule.models.Instructor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InstructorRepository extends JpaRepository<Instructor, Long> {
-}
